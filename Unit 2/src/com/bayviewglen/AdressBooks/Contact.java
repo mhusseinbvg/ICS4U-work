9c7e29a98f44dec2ea37e862587a6325df538316
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-22 15:35:58 -0400

    Made getter and setter methods for contact class

--- Unit 2/src/com/bayviewglen/AdressBooks/Contact.java
+++ Unit 2/src/com/bayviewglen/AdressBooks/Contact.java
@@ -17,4 +17,27 @@ public class Contact {
 		this.phone  = phone;
 	}
 	
+	public String getLastName(){
+		return lname;
+	}
+	
+	public String getFirstName(){
+		return fname;
+	}
+	
+	public String getPhone(){
+		return phone;
+	}
+	
+	public void changeLastName(String last){
+		lname = last;
+	}
+	
+	public void changeFirstName(String first){
+		fname = first;
+	}
+	
+	public void changePhone(String phone){
+		this.phone = phone;
+	}
 }

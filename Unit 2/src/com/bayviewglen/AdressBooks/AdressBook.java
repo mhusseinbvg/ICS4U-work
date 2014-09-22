49f36763234a0bdea7f71f37f2d1cbee33cc3fc8
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-22 15:22:53 -0400

    Initial commit to get it all started

---
+++ Unit 2/src/com/bayviewglen/AdressBooks/AdressBook.java
@@ -0,0 +1,5 @@
+package com.bayviewglen.AdressBooks;
+
+public class AdressBook {
+
+}
---
+++ Unit 2/src/com/bayviewglen/AdressBooks/Contact.java
@@ -0,0 +1,14 @@
+package com.bayviewglen.AdressBooks;
+
+public class Contact {
+	private String lname;
+	private String fname;
+	private String phone;
+	
+	public Contact(String last, String first, String phone){
+		lname = last;
+		fname = first;
+		this.phone  = phone;
+	}
+
+}

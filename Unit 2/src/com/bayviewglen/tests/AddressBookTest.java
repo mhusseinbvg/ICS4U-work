c3d71968cb21dee266a3a476786b0596160b9b68
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-23 20:32:28 -0400

    Made test cases for AddressBook but am not sure how to test these

---
+++ Unit 2/src/com/bayviewglen/tests/AddressBookTest.java
@@ -0,0 +1,34 @@
+package com.bayviewglen.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddressBookTest {
+	
+	@Test
+	public void testAddAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayAll() {
+		fail("Not yet implemented");
+	}
+
+}

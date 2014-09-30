7bb616a3fca0745f9533084298f0667aef03cbe1
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-30 14:04:17 -0400

    added some variables

--- Unit 2/src/com/bayviewglen/QueensAssignment/Solution.java
+++ Unit 2/src/com/bayviewglen/QueensAssignment/Solution.java
@@ -1,8 +1,16 @@
 package com.bayviewglen.QueensAssignment;
 
+import java.util.Stack;
+
 public class Solution {
+	int queenNumber;
+	int boardSize;
+	Stack<Queen> queenStack = new Stack<Queen>();
+	
 	
 	public boolean checkForSolution(){
 		return true;
 	}
+	
+	
 }

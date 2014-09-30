22b1730ca02e46606cee1b63137333f2a8fccf64
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-29 20:12:56 -0400

    Got the queen class completly finished

--- Unit 2/src/com/bayviewglen/QueensAssignment/Queen.java
+++ Unit 2/src/com/bayviewglen/QueensAssignment/Queen.java
@@ -1,5 +1,30 @@
 package com.bayviewglen.QueensAssignment;
 
 public class Queen {
-
+	private int row;
+	private int column;
+	
+	public Queen(){
+	}
+	
+	public Queen(int r, int c){
+		row = r;
+		column = c;
+	}
+	
+	public void changeRow(int r){
+		row = r;
+	}
+	
+	public void changeColumn(int c){
+		column = c;
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getColumn(){
+		return column;
+	}
 }

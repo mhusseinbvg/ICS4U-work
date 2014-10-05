8704131003e6a10f47226764e13afa246c069d3d
mhusseinbvg <mhussein@bayviewglen.ca>
2014-10-05 11:01:39 -0400

    Added a few method stubs

--- Unit 2/src/com/bayviewglen/QueensAssignment/Solution.java
+++ Unit 2/src/com/bayviewglen/QueensAssignment/Solution.java
@@ -5,12 +5,34 @@ import java.util.Stack;
 public class Solution {
 	int queenNumber;
 	int boardSize;
+	Queen[] queens;
 	Stack<Queen> queenStack = new Stack<Queen>();
-	
-	
+
+	public Solution(int numOfStuff){
+		queenNumber = numOfStuff;
+		boardSize = numOfStuff;
+		queens = new Queen[numOfStuff];
+
+
+	}
+
 	public boolean checkForSolution(){
+		if(checkHorizontal() && checkVertical() && checkDiagonal())
+			return true;
+		else
+			return false;
+	}
+
+	public boolean checkHorizontal(){
 		return true;
 	}
-	
-	
+
+	public boolean checkVertical(){
+		return true;
+	}
+
+	public boolean checkDiagonal(){
+		return true;
+	}
+
 }

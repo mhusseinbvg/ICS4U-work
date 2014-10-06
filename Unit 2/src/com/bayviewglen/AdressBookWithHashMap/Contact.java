58c92e9685a74d34986eccef5cd57a1593ede2e6
mhusseinbvg <mhussein@bayviewglen.ca>
2014-10-06 13:24:41 -0400

    Contact class copied over

--- Unit 2/src/com/bayviewglen/AdressBookWithHashMap/Contact.java
+++ Unit 2/src/com/bayviewglen/AdressBookWithHashMap/Contact.java
@@ -1,5 +1,47 @@
 package com.bayviewglen.AdressBookWithHashMap;
 
 public class Contact {
+	private String lname;
+	private String fname;
+	private String phone;
+
+	public Contact(){
+		lname = null;
+		fname = null;
+		phone = null;
+	}
+
+	public Contact(String last, String first, String phone){
+		lname = last;
+		fname = first;
+		this.phone  = phone;
+	}
+
+	public String getLastName(){
+		return lname;
+	}
+
+	public String getFirstName(){
+		return fname;
+	}
+
+	public String getPhone(){
+		return phone;
+	}
+
+	public void changeLastName(String last){
+		lname = last;
+	}
+
+	public void changeFirstName(String first){
+		fname = first;
+	}
+
+	public void changePhone(String phone){
+		this.phone = phone;
+	}
+	public void displayContact(){
+		System.out.println(lname + ", " + fname + ": " + phone);
+	}
 
 }
